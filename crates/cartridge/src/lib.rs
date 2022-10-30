65d7ba6a815f38f2ef690520acb44c52dcf70b6b
nomyfan <Nomyfan@live.com>
2022-10-31 01:26:26 +0800

    chore: move intergration tests

--- crates/cartridge/src/lib.rs
+++ crates/cartridge/src/lib.rs
@@ -400,30 +400,3 @@ impl Cartridge {
         Ok(Cartridge { header, rom })
     }
 }
-
-#[cfg(test)]
-mod tests {
-    const ROMS: [&str; 14] = [
-        "../../roms/01-special.gb",
-        "../../roms/02-interrupts.gb",
-        "../../roms/03-op sp,hl.gb",
-        "../../roms/04-op r,imm.gb",
-        "../../roms/05-op rp.gb",
-        "../../roms/06-ld r,r.gb",
-        "../../roms/07-jr,jp,call,ret,rst.gb",
-        "../../roms/08-misc instrs.gb",
-        "../../roms/09-op r,r.gb",
-        "../../roms/10-bit ops.gb",
-        "../../roms/11-op a,(hl).gb",
-        "../../roms/cpu_instrs.gb",
-        "../../roms/dmg-acid2.gb",
-        "../../roms/mem_timing.gb",
-    ];
-
-    #[test]
-    fn test_load() {
-        ROMS.iter().for_each(|rom| {
-            super::Cartridge::load(std::path::Path::new(rom)).unwrap();
-        })
-    }
-}
