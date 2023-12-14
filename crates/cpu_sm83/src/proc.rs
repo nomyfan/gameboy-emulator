6d8a7865a3e937c87d5abb484960d5def77dfaf2
nomyfan <Nomyfan@live.com>
2023-12-14 22:18:05 +0800

    fix: inc & add tests

--- crates/cpu_sm83/src/proc.rs
+++ crates/cpu_sm83/src/proc.rs
@@ -38,11 +38,13 @@ fn cpu_write_a(cpu: &mut impl Cpu16, value: u8) {
 }
 
 pub(crate) fn proc_inc(cpu: &mut impl Cpu16, opcode: u8, am: &AddressingMode) -> u8 {
-    let value = cpu.fetch_data(am);
-    let value = value.wrapping_add(1);
+    let mut value = cpu.fetch_data(am);
 
-    if opcode != 0x03 && opcode != 0x13 && opcode != 0x23 && opcode != 0x33 {
+    if (opcode & 0x03) != 0x03 {
+        value = (value as u8).wrapping_add(1) as u16;
         cpu.set_flags(Some(value == 0), Some(false), Some((value & 0xF) == 0), None);
+    } else {
+        value = value.wrapping_add(1);
     }
 
     cpu.write_data(am, 0, value);
@@ -694,6 +696,54 @@ mod tests {
                 mock.expect_bus_read().with(eq(addr)).once().returning(move |_| value);
 
                 assert_eq!(proc_ld(&mut mock, opcode, &am1, &am2), get_cycles(opcode).0);
+            }
+        }
+    }
+
+    mod proc_inc {
+        use super::*;
+
+        #[test]
+        fn inc_rr() {
+            let cases = [
+                (0x03u8, AM::Direct_BC, 0xFFFFu16, 0u16),
+                (0x13, AM::Direct_DE, 0xFFFF, 0),
+                (0x23, AM::Direct_HL, 0xFFFF, 0),
+                (0x33, AM::Direct_SP, 0xFFFF, 0),
+            ];
+
+            for (opcode, am, val, ret) in cases {
+                let mut mock = MockCpu16::new();
+                mock.expect_fetch_data().with(eq(am)).once().returning(move |_| val);
+                mock.expect_write_data()
+                    .with(eq(am), always(), eq(ret))
+                    .once()
+                    .returning(|_, _, _| {});
+
+                assert_eq!(proc_inc(&mut mock, opcode, &am), get_cycles(opcode).0);
+            }
+        }
+
+        #[test]
+        fn inc_set_flags() {
+            let cases = [
+                (0x04u8, AM::Direct_B, 0xFFu8, 0u16, (true, true)),
+                (0x34, AM::Indirect_HL, 0xF, 0x10, (false, true)),
+            ];
+
+            for (opcode, am, val, ret, (z, h)) in cases {
+                let mut mock = MockCpu16::new();
+                mock.expect_fetch_data().with(eq(am)).once().returning(move |_| val as u16);
+                mock.expect_write_data()
+                    .with(eq(am), always(), eq(ret))
+                    .once()
+                    .returning(|_, _, _| {});
+                mock.expect_set_flags()
+                    .with(eq(Some(z)), eq(Some(false)), eq(Some(h)), eq(None))
+                    .once()
+                    .returning(|_, _, _, _| {});
+
+                assert_eq!(proc_inc(&mut mock, opcode, &am), get_cycles(opcode).0);
             }
         }
     }
