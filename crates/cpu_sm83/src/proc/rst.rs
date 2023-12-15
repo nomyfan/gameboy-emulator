7b24e6d1586d6a31a78ff12f03dabc1f6a95d8e0
nomyfan <Nomyfan@live.com>
2023-12-16 01:50:01 +0800

    refactor: bitwise

--- crates/cpu_sm83/src/proc/rst.rs
+++ crates/cpu_sm83/src/proc/rst.rs
@@ -3,7 +3,8 @@ use crate::instruction::get_cycles;
 
 pub(crate) fn proc_rst(cpu: &mut impl Cpu16, opcode: u8) -> u8 {
     let addrs = [0x00, 0x08, 0x10, 0x18, 0x20, 0x28, 0x30, 0x38];
-    let addr = addrs[((opcode - 0xC7) / 8) as usize];
+    // 11xxx111
+    let addr = addrs[((opcode & 0x38) >> 3) as usize];
     cpu.stack_push_pc();
     cpu.jp(addr);
 
