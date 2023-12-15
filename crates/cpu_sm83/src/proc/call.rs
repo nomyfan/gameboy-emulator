79a970c3b9a93ec61c82810b0e7f09b8f6aa9bed
nomyfan <Nomyfan@live.com>
2023-12-15 20:32:57 +0800

    test: add tests for CALL

--- crates/cpu_sm83/src/proc/call.rs
+++ crates/cpu_sm83/src/proc/call.rs
@@ -13,3 +13,43 @@ pub(crate) fn proc_call(cpu: &mut impl Cpu16, opcode: u8, cond: &Option<Conditio
 
     get_cycles(opcode).1
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::cpu16::MockCpu16;
+    use crate::instruction::get_cycles;
+    use mockall::predicate::*;
+
+    #[test]
+    fn call_condition_success() {
+        let cases = [
+            (0xD4u8, Some(Condition::NC), (false, false, false, false)),
+            (0xCD, None, (false, false, false, false)),
+        ];
+
+        for (opcode, cond, flags) in cases.into_iter() {
+            let mut mock = MockCpu16::new();
+            let addr = 0x1234u16;
+            mock.expect_fetch_data().once().with(eq(AddressingMode::PC2)).return_const(addr);
+            mock.expect_flags().times(if cond.is_none() { 0 } else { 1 }).returning(move || flags);
+            mock.expect_stack_push_pc().once().return_const(());
+            mock.expect_jp().once().with(eq(addr)).return_const(());
+
+            assert_eq!(proc_call(&mut mock, opcode, &cond), get_cycles(opcode).0);
+        }
+    }
+
+    #[test]
+    fn call_condition_fail() {
+        let opcode = 0xDCu8;
+        let addr = 0x1234u16;
+
+        let mut mock = MockCpu16::new();
+        mock.expect_fetch_data().once().with(eq(AddressingMode::PC2)).return_const(addr);
+        mock.expect_flags().once().returning(|| (false, false, false, false));
+        mock.expect_stack_push_pc().never();
+
+        assert_eq!(proc_call(&mut mock, opcode, &Some(Condition::C)), get_cycles(opcode).1);
+    }
+}
