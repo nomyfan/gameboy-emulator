aa838d3dde66d12c3396e4beacca02ca7aa3888f
nomyfan <Nomyfan@live.com>
2023-12-16 11:26:24 +0800

    refactor: no casting

--- crates/cpu_sm83/src/proc/sub.rs
+++ crates/cpu_sm83/src/proc/sub.rs
@@ -3,13 +3,13 @@ use crate::cpu16::Cpu16;
 use crate::instruction::{get_cycles, AddressingMode};
 
 pub(crate) fn proc_sub(cpu: &mut impl Cpu16, opcode: u8, am: &AddressingMode) -> u8 {
-    let data = cpu.fetch_data(am);
+    let data = cpu.fetch_data(am) as u8;
     let a = cpu_fetch_a(cpu);
-    let value = a.wrapping_sub(data as u8);
+    let value = a.wrapping_sub(data);
 
     let z = value == 0;
-    let h = ((a & 0xF) as i16) - ((data & 0xF) as i16) < 0;
-    let c = (a as i16) - (data as i16) < 0;
+    let h = (a & 0xF) < (data & 0xF);
+    let c = a < data;
 
     cpu_write_a(cpu, value);
     cpu.set_flags(Some(z), Some(true), Some(h), Some(c));
