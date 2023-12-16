563ea8d4f95f7633b83aaa9b5503be423338cc10
nomyfan <Nomyfan@live.com>
2023-12-16 11:54:46 +0800

    test: add tests for CPL

--- crates/cpu_sm83/src/proc/cpl.rs
+++ crates/cpu_sm83/src/proc/cpl.rs
@@ -9,3 +9,29 @@ pub(crate) fn proc_cpl(cpu: &mut impl Cpu16, opcode: u8) -> u8 {
 
     get_cycles(opcode).0
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::cpu16::MockCpu16;
+    use crate::instruction::AddressingMode as AM;
+    use mockall::predicate::*;
+
+    #[test]
+    fn cpl() {
+        let am = AM::Direct_A;
+        let cases = [(1u16, 0xFEu16), (0, 0xFF)];
+
+        for (a, ret) in cases.into_iter() {
+            let mut mock = MockCpu16::new();
+            mock.expect_fetch_data().with(eq(am)).once().return_const(a);
+            mock.expect_write_data().with(eq(am), always(), eq(ret)).once().return_const(());
+            mock.expect_set_flags()
+                .once()
+                .with(eq(None), eq(Some(true)), eq(Some(true)), eq(None))
+                .return_const(());
+
+            proc_cpl(&mut mock, 0x2F);
+        }
+    }
+}
