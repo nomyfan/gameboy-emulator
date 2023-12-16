853c04d369766d5d16c0d5fa3127e77fc704603e
nomyfan <Nomyfan@live.com>
2023-12-16 10:37:37 +0800

    test: add tests for SUB,CP

--- crates/cpu_sm83/src/proc/cp.rs
+++ crates/cpu_sm83/src/proc/cp.rs
@@ -10,3 +10,36 @@ pub(crate) fn proc_cp(cpu: &mut impl Cpu16, opcode: u8, am: &AddressingMode) -> 
 
     get_cycles(opcode).0
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::cpu16::MockCpu16;
+
+    use super::*;
+    use mockall::predicate::*;
+    type AM = AddressingMode;
+
+    #[test]
+    fn cp() {
+        let opcode = 0xFEu8;
+        let am = AM::PC1;
+        let cases = [
+            (0x11u16, 0x21u16, (false, false, true)),
+            (1, 1, (true, false, false)),
+            (2, 1, (false, false, false)),
+            (1, 3, (false, true, true)),
+        ];
+
+        for (a, v, (z, h, c)) in cases.into_iter() {
+            let mut mock = MockCpu16::new();
+            mock.expect_fetch_data().with(eq(AM::Direct_A)).once().return_const(a);
+            mock.expect_fetch_data().with(eq(am)).once().return_const(v);
+            mock.expect_set_flags()
+                .once()
+                .with(eq(Some(z)), eq(Some(true)), eq(Some(h)), eq(Some(c)))
+                .return_const(());
+
+            proc_cp(&mut mock, opcode, &am);
+        }
+    }
+}
--- crates/cpu_sm83/src/proc/sub.rs
+++ crates/cpu_sm83/src/proc/sub.rs
@@ -16,3 +16,38 @@ pub(crate) fn proc_sub(cpu: &mut impl Cpu16, opcode: u8, am: &AddressingMode) ->
 
     get_cycles(opcode).0
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use mockall::predicate::*;
+
+    use crate::cpu16::MockCpu16;
+    type AM = AddressingMode;
+
+    #[test]
+    fn sub() {
+        let opcode = 0xD6u8;
+        let am1 = AM::Direct_A;
+        let am2 = AM::PC1;
+
+        let cases = [
+            (1u16, 1u16, 0u16, (true, false, false)),
+            (2, 1, 1, (false, false, false)),
+            (1, 3, -2i8 as u8 as u16, (false, true, true)),
+            (0x11, 0x21, 0xF0, (false, false, true)),
+        ];
+
+        for (a, v, ret, (z, h, c)) in cases.into_iter() {
+            let mut mock = MockCpu16::new();
+            mock.expect_fetch_data().with(eq(am1)).once().return_const(a);
+            mock.expect_fetch_data().with(eq(am2)).once().return_const(v);
+            mock.expect_write_data().with(eq(am1), always(), eq(ret)).once().return_const(());
+            mock.expect_set_flags()
+                .once()
+                .with(eq(Some(z)), eq(Some(true)), eq(Some(h)), eq(Some(c)))
+                .return_const(());
+            proc_sub(&mut mock, opcode, &am2);
+        }
+    }
+}
