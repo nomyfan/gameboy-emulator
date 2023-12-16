924dedfd696fdda9710f55ad74fee73801cc311b
nomyfan <Nomyfan@live.com>
2023-12-16 18:03:51 +0800

    test: add tests for RLA,RLCA,RRA,RRCA

--- crates/cpu_sm83/src/proc/rla.rs
+++ crates/cpu_sm83/src/proc/rla.rs
@@ -12,3 +12,36 @@ pub(crate) fn proc_rla(cpu: &mut impl Cpu16, opcode: u8) -> u8 {
 
     get_cycles(opcode).0
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{cpu16::MockCpu16, instruction::AddressingMode as AM};
+    use mockall::predicate::*;
+
+    #[test]
+    fn rla() {
+        let opcode = 0x17;
+        let am = AM::Direct_A;
+
+        let cases = [
+            ((0b1000_1000u16, true), (0b0001_0001, true)),
+            ((0b1000_1000, false), (0b0001_0000, true)),
+            ((0b0000_1000, true), (0b0001_0001, false)),
+            ((0b0000_1000, false), (0b0001_0000, false)),
+        ];
+
+        for ((in_a, in_flag_c), (out_a, out_flag_c)) in cases.into_iter() {
+            let mut mock = MockCpu16::new();
+            mock.expect_fetch_data().with(eq(am)).once().return_const(in_a);
+            mock.expect_flags().once().return_const([false, false, false, in_flag_c]);
+            mock.expect_write_data().with(eq(am), always(), eq(out_a)).once().return_const(());
+            mock.expect_set_flags()
+                .once()
+                .with(eq(Some(false)), eq(Some(false)), eq(Some(false)), eq(Some(out_flag_c)))
+                .return_const(());
+
+            proc_rla(&mut mock, opcode);
+        }
+    }
+}
--- crates/cpu_sm83/src/proc/rlca.rs
+++ crates/cpu_sm83/src/proc/rlca.rs
@@ -12,3 +12,35 @@ pub(crate) fn proc_rlca(cpu: &mut impl Cpu16, opcode: u8) -> u8 {
 
     get_cycles(opcode).0
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::cpu16::MockCpu16;
+    use crate::instruction::AddressingMode as AM;
+    use mockall::predicate::*;
+
+    #[test]
+    fn rlca() {
+        let opcode = 0x0F;
+        let am = AM::Direct_A;
+
+        let cases = [
+            //
+            ((0b1001_0000u16), (0b0010_0001, true)),
+            ((0b0001_0001), (0b0010_0010, false)),
+        ];
+
+        for (in_a, (out_a, out_flag_c)) in cases.into_iter() {
+            let mut mock = MockCpu16::new();
+            mock.expect_fetch_data().with(eq(am)).once().return_const(in_a);
+            mock.expect_write_data().with(eq(am), always(), eq(out_a)).once().return_const(());
+            mock.expect_set_flags()
+                .once()
+                .with(eq(Some(false)), eq(Some(false)), eq(Some(false)), eq(Some(out_flag_c)))
+                .return_const(());
+
+            proc_rlca(&mut mock, opcode);
+        }
+    }
+}
