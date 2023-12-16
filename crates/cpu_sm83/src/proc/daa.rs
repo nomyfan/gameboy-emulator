8fd0cd3af145f6522571a3453b5ba7a7ccb2a3af
nomyfan <Nomyfan@live.com>
2023-12-16 19:54:42 +0800

    test: add tests for DAA

--- crates/cpu_sm83/src/proc/daa.rs
+++ crates/cpu_sm83/src/proc/daa.rs
@@ -2,6 +2,7 @@ use super::utils::{cpu_fetch_a, cpu_write_a};
 use crate::cpu16::Cpu16;
 use crate::instruction::get_cycles;
 
+/// Decimal adjust A
 pub(crate) fn proc_daa(cpu: &mut impl Cpu16, opcode: u8) -> u8 {
     let mut acc = 0;
     let mut c = false;
@@ -9,8 +10,8 @@ pub(crate) fn proc_daa(cpu: &mut impl Cpu16, opcode: u8) -> u8 {
 
     let (_, flag_n, flag_h, flag_c) = cpu.flags();
 
-    if flag_h || (!flag_n && (a & 0xF) > 0x09) {
-        acc |= 0x06;
+    if flag_h || (!flag_n && (a & 0xF) > 0x9) {
+        acc |= 0x6;
     }
 
     if flag_c || (!flag_n && a > 0x99) {
@@ -23,4 +24,109 @@ pub(crate) fn proc_daa(cpu: &mut impl Cpu16, opcode: u8) -> u8 {
     cpu.set_flags(Some(value == 0), None, Some(false), Some(c));
 
     get_cycles(opcode).0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::cpu16::MockCpu16;
+    use crate::instruction::AddressingMode as Am;
+    use mockall::predicate::*;
+
+    const OPCODE: u8 = 0x27;
+    const AM: Am = Am::Direct_A;
+
+    #[test]
+    fn daa_flag_h_set_then_add_0x6() {
+        let mut mock = MockCpu16::new();
+        mock.expect_fetch_data().with(eq(AM)).once().return_const(0x00u16);
+        mock.expect_flags().once().return_const([false, false, true, false]);
+        mock.expect_write_data().with(eq(AM), always(), eq(0x6)).once().return_const(());
+        mock.expect_set_flags()
+            .with(eq(Some(false)), eq(None), eq(Some(false)), eq(Some(false)))
+            .return_const(());
+
+        proc_daa(&mut mock, OPCODE);
+    }
+
+    #[test]
+    fn daa_reg_a_over_0x9_then_add_0x6() {
+        let mut mock = MockCpu16::new();
+        mock.expect_fetch_data().with(eq(AM)).once().return_const(0xAu16);
+        mock.expect_flags().once().return_const([false, false, false, false]);
+        mock.expect_write_data().with(eq(AM), always(), eq(0xA + 0x6)).once().return_const(());
+        mock.expect_set_flags()
+            .with(eq(Some(false)), eq(None), eq(Some(false)), eq(Some(false)))
+            .return_const(());
+
+        proc_daa(&mut mock, OPCODE);
+    }
+
+    #[test]
+    fn daa_flag_c_set_then_add_0x60() {
+        let mut mock = MockCpu16::new();
+        mock.expect_fetch_data().with(eq(AM)).once().return_const(0x00u16);
+        mock.expect_flags().once().return_const([false, false, false, true]);
+        mock.expect_write_data().with(eq(AM), always(), eq(0x60)).once().return_const(());
+        mock.expect_set_flags()
+            .with(eq(Some(false)), eq(None), eq(Some(false)), eq(Some(true)))
+            .return_const(());
+
+        proc_daa(&mut mock, OPCODE);
+    }
+
+    #[test]
+    fn daa_reg_a_over_0x90_then_add_0x60() {
+        let mut mock = MockCpu16::new();
+        mock.expect_fetch_data().with(eq(AM)).once().return_const(0xB0u16);
+        mock.expect_flags().once().return_const([false, false, false, false]);
+        mock.expect_write_data()
+            .with(eq(AM), always(), eq(0xB0u8.wrapping_add(0x60) as u16))
+            .once()
+            .return_const(());
+        mock.expect_set_flags()
+            .with(eq(Some(false)), eq(None), eq(Some(false)), eq(Some(true)))
+            .return_const(());
+
+        proc_daa(&mut mock, OPCODE);
+    }
+
+    #[test]
+    fn daa_flag_n_set_then_subtract() {
+        let mut mock = MockCpu16::new();
+        mock.expect_fetch_data().with(eq(AM)).once().return_const(0xAAu16);
+        mock.expect_flags().once().return_const([false, true, true, true]);
+        mock.expect_write_data()
+            .with(eq(AM), always(), eq(0xAAu8.wrapping_sub(0x66) as u16))
+            .once()
+            .return_const(());
+        mock.expect_set_flags()
+            .with(eq(Some(false)), eq(None), eq(Some(false)), eq(Some(true)))
+            .return_const(());
+
+        proc_daa(&mut mock, OPCODE);
+    }
+
+    #[test]
+    fn daa_set_flag_z_false() {
+        let cases = [
+            //
+            ((0x66u16, (true, true, true)), (true)),
+            (((-0x66i8) as u8 as u16, (false, true, true)), (true)),
+            ((0, (false, false, false)), (false)),
+            ((0, (true, false, false)), (false)),
+        ];
+
+        for ((in_a, (in_n, in_h, in_c)), out_c) in cases.into_iter() {
+            let mut mock = MockCpu16::new();
+            mock.expect_fetch_data().with(eq(AM)).once().return_const(in_a);
+            mock.expect_flags().once().return_const([false, in_n, in_h, in_c]);
+            mock.expect_write_data().with(eq(AM), always(), eq(0)).once().return_const(());
+            mock.expect_set_flags()
+                .with(eq(Some(true)), eq(None), eq(Some(false)), eq(Some(out_c)))
+                .return_const(());
+
+            proc_daa(&mut mock, OPCODE);
+        }
+    }
 }
