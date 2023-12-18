cd640c2fae80c1a21b93b7193cb0dd02adc6dd8b
nomyfan <Nomyfan@live.com>
2023-12-18 15:47:02 +0800

    test: add tests for Timer

--- crates/gb/src/timer.rs
+++ crates/gb/src/timer.rs
@@ -1,4 +1,4 @@
-use gb_shared::{is_bit_set, Memory};
+use gb_shared::{is_bit_set, InterruptRequest, Memory};
 
 enum CounterIncCycles {
     Cycles1024,
@@ -19,7 +19,7 @@ impl From<u8> for CounterIncCycles {
     }
 }
 
-pub(crate) struct Timer<BUS: Memory> {
+pub(crate) struct Timer<INT: InterruptRequest> {
     cycles_so_far: u16,
     /// Divider
     /// It's increased at a rate of 16384 Hz.
@@ -38,10 +38,10 @@ pub(crate) struct Timer<BUS: Memory> {
     ///     - 10: 65536 Hz(increased by 1 every 64 cycles)
     ///     - 11: 16384 Hz(increased by 1 every 256 cycles)
     tac: u8,
-    bus: BUS,
+    interrupt_request: INT,
 }
 
-impl<BUS: Memory> Memory for Timer<BUS> {
+impl<INT: InterruptRequest> Memory for Timer<INT> {
     fn write(&mut self, addr: u16, value: u8) {
         if addr == 0xFF04 {
             // Write any value to it will reset it to zero.
@@ -72,9 +72,9 @@ impl<BUS: Memory> Memory for Timer<BUS> {
     }
 }
 
-impl<BUS: Memory> Timer<BUS> {
-    pub fn new(bus: BUS) -> Self {
-        Self { div: 0, tima: 0, tma: 0, tac: 0, cycles_so_far: 0, bus }
+impl<INT: InterruptRequest> Timer<INT> {
+    pub fn new(interrupt_request: INT) -> Self {
+        Self { div: 0, tima: 0, tma: 0, tac: 0, cycles_so_far: 0, interrupt_request }
     }
 
     pub fn step(&mut self, cycles: u8) {
@@ -96,44 +96,158 @@ impl<BUS: Memory> Timer<BUS> {
             self.tima = self.tima.wrapping_add(1);
             if self.tima == 0 {
                 self.tima = self.tma;
-                // TODO: refactor request interrupt impl
-                self.bus.write(0xFF0F, self.bus.read(0xFF0F) | 0b100);
+                self.interrupt_request.request_timer();
             }
         }
     }
 }
 
 #[cfg(test)]
+use gb_shared::InterruptType;
+#[cfg(test)]
+use mockall::mock;
+
+#[cfg(test)]
+mock! {
+    pub InterruptRequest {}
+
+    impl InterruptRequest for InterruptRequest {
+        fn request(&mut self, interrupt_type: InterruptType) {}
+    }
+}
+
+#[cfg(test)]
 mod tests {
+    use core::time;
+    use mockall::predicate::*;
+
     use super::*;
+
+    fn write_div(timer: &mut Timer<MockInterruptRequest>, value: u8) {
+        timer.write(0xFF04, value);
+    }
+
+    fn write_tima(timer: &mut Timer<MockInterruptRequest>, value: u8) {
+        timer.write(0xFF05, value);
+    }
+
+    fn write_tma(timer: &mut Timer<MockInterruptRequest>, value: u8) {
+        timer.write(0xFF06, value);
+    }
+
+    fn write_tac(timer: &mut Timer<MockInterruptRequest>, value: u8) {
+        timer.write(0xFF07, value);
+    }
 
     #[test]
     fn div_increased_ignoring_tac() {
-        //
+        let mut timer = Timer::new(MockInterruptRequest::new());
+        write_tac(&mut timer, 0);
+
+        timer.step(255);
+        assert_eq!(timer.cycles_so_far, 255);
+        assert_eq!(timer.div, 0);
+
+        timer.step(1);
+        assert_eq!(timer.cycles_so_far, 256);
+        assert_eq!(timer.div, 1);
     }
 
     #[test]
     fn tima_increased_every_1024_cycles() {
-        //
+        let mut timer = Timer::new(MockInterruptRequest::new());
+        write_tac(&mut timer, 0b100);
+
+        for _ in 0..4 {
+            timer.step(255);
+        }
+        assert_eq!(timer.cycles_so_far, 1020);
+        assert_eq!(timer.tima, 0);
+
+        timer.step(4);
+        assert_eq!(timer.cycles_so_far, 1024);
+        assert_eq!(timer.tima, 1);
+
+        for _ in 0..4 {
+            timer.step(255);
+        }
+        assert_eq!(timer.cycles_so_far, 2044);
+        assert_eq!(timer.tima, 1);
+
+        timer.step(4);
+        assert_eq!(timer.cycles_so_far, 2048);
+        assert_eq!(timer.tima, 2);
     }
 
     #[test]
     fn tima_increased_every_16_cycles() {
-        //
+        let mut timer = Timer::new(MockInterruptRequest::new());
+        write_tac(&mut timer, 0b101);
+
+        timer.step(16);
+        assert_eq!(timer.tima, 1);
+
+        timer.step(15);
+        assert_eq!(timer.tima, 1);
+        timer.step(1);
+        assert_eq!(timer.tima, 2);
     }
 
     #[test]
     fn tima_increased_every_64_cycles() {
-        //
+        let mut timer = Timer::new(MockInterruptRequest::new());
+        write_tac(&mut timer, 0b110);
+
+        timer.step(64);
+        assert_eq!(timer.tima, 1);
+
+        timer.step(63);
+        assert_eq!(timer.tima, 1);
+        timer.step(1);
+        assert_eq!(timer.tima, 2);
     }
 
     #[test]
     fn tima_increased_every_256_cycles() {
-        //
+        let mut timer = Timer::new(MockInterruptRequest::new());
+        write_tac(&mut timer, 0b111);
+
+        timer.step(255);
+        assert_eq!(timer.tima, 0);
+        timer.step(1);
+        assert_eq!(timer.tima, 1);
+
+        timer.step(255);
+        assert_eq!(timer.tima, 1);
+        timer.step(1);
+        assert_eq!(timer.tima, 2);
+    }
+
+    #[test]
+    fn tima_not_increased_if_tima_is_disabled() {
+        let mut timer = Timer::new(MockInterruptRequest::new());
+        write_tac(&mut timer, 0b001);
+
+        timer.step(16);
+        assert_eq!(timer.tima, 0);
+
+        timer.step(16);
+        assert_eq!(timer.tima, 0);
     }
 
     #[test]
     fn request_timer_interrupt() {
-        //
+        let mut mock_int_req = MockInterruptRequest::new();
+        mock_int_req.expect_request().with(eq(InterruptType::Timer)).once().return_const(());
+
+        let mut timer = Timer::new(mock_int_req);
+        write_tma(&mut timer, 0xFE);
+        write_tima(&mut timer, 0xFF);
+        write_tac(&mut timer, 0b111);
+
+        timer.step(255);
+        timer.step(1);
+
+        assert_eq!(timer.tima, 0xFE);
     }
 }
--- crates/shared/src/lib.rs
+++ crates/shared/src/lib.rs
@@ -24,3 +24,31 @@ pub trait Memory {
     fn write(&mut self, addr: u16, value: u8);
     fn read(&self, addr: u16) -> u8;
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum InterruptType {
+    VBlank = 0b1,
+    LCDStat = 0b10,
+    Timer = 0b100,
+    Serial = 0b1000,
+    Joypad = 0b10000,
+}
+
+pub trait InterruptRequest {
+    fn request(&mut self, interrupt_type: InterruptType);
+    fn request_vblank(&mut self) {
+        self.request(InterruptType::VBlank);
+    }
+    fn request_lcd_stat(&mut self) {
+        self.request(InterruptType::LCDStat);
+    }
+    fn request_timer(&mut self) {
+        self.request(InterruptType::Timer);
+    }
+    fn request_serial(&mut self) {
+        self.request(InterruptType::Serial);
+    }
+    fn request_joypad(&mut self) {
+        self.request(InterruptType::Joypad);
+    }
+}
