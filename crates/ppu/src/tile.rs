07d458a0168b1f276b935b16a74ee3bdb10cb4ec
nomyfan <Nomyfan@live.com>
2024-01-11 23:04:31 +0800

    wip: delete dead code

--- crates/ppu/src/tile.rs
+++ crates/ppu/src/tile.rs
@@ -19,25 +19,6 @@ impl TileData {
     }
 }
 
-pub(crate) trait TileDataBuilder {
-    fn low(&mut self, data: [u8; 8]) -> &mut Self;
-    fn high(&mut self, data: [u8; 8]) -> &mut Self;
-    fn build(self) -> TileData;
-}
-
-#[derive(Debug, Default)]
-pub(crate) struct BackgroundTileDataBuilder {
-    pub(crate) index: u8,
-    low: Option<[u8; 8]>,
-    high: Option<[u8; 8]>,
-}
-
-impl BackgroundTileDataBuilder {
-    pub(crate) fn new(index: u8) -> Self {
-        BackgroundTileDataBuilder { index, low: None, high: None }
-    }
-}
-
 pub(crate) fn mix_colors(low: &[u8; 8], high: &[u8; 8]) -> [u16; 8] {
     let mut colors: [u16; 8] = Default::default();
 
@@ -87,68 +68,6 @@ pub(crate) fn apply_attrs(data: &mut [u16; 8], attrs: &ObjectAttrs) {
     }
 }
 
-impl TileDataBuilder for BackgroundTileDataBuilder {
-    fn low(&mut self, data: [u8; 8]) -> &mut Self {
-        self.low = Some(data);
-        self
-    }
-
-    fn high(&mut self, data: [u8; 8]) -> &mut Self {
-        self.high = Some(data);
-        self
-    }
-
-    fn build(self) -> TileData {
-        let Some(low) = self.low else { panic!("low data is not set") };
-        let Some(high) = self.high else { panic!("high data is not set") };
-
-        let colors = mix_colors(&low, &high);
-        TileData { colors, object: None }
-    }
-}
-
-#[derive(Debug, Default)]
-pub(crate) struct ObjectTileDataBuilder {
-    object: Object,
-    tile_index: u8,
-    low: Option<[u8; 8]>,
-    high: Option<[u8; 8]>,
-}
-
-impl ObjectTileDataBuilder {
-    pub(crate) fn new(object: Object, object_size: u8) -> Self {
-        // Remove the last bit.
-        let tile_index =
-            if object_size == 16 { object.tile_index & 0b1111_1110 } else { object.tile_index };
-        ObjectTileDataBuilder { object, low: None, high: None, tile_index }
-    }
-
-    pub(crate) fn tile_index(&self) -> u8 {
-        self.tile_index
-    }
-}
-
-impl TileDataBuilder for ObjectTileDataBuilder {
-    fn low(&mut self, data: [u8; 8]) -> &mut Self {
-        self.low = Some(data);
-        self
-    }
-
-    fn high(&mut self, data: [u8; 8]) -> &mut Self {
-        self.high = Some(data);
-        self
-    }
-
-    fn build(self) -> TileData {
-        let Some(low) = self.low else { panic!("low data is not set") };
-        let Some(high) = self.high else { panic!("high data is not set") };
-
-        let mut colors = mix_colors(&low, &high);
-        apply_attrs(&mut colors, &self.object.attrs);
-        TileData { colors, object: Some(self.object) }
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::{apply_attrs, mix_colors, TileData};
@@ -177,7 +96,7 @@ mod tests {
             0b01_11_11_00,
         ];
 
-        let colors = mix_colors(low, high);
+        let colors = mix_colors(&low, &high);
 
         let expected: [u16; 8] = [
             0b00_10_11_11_11_11_10_00,
