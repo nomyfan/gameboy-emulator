9a71524ee58136bfa84b38a3ac2a49875fb8751f
nomyfan <Nomyfan@live.com>
2024-03-03 22:25:23 +0800

    wip: fix typo

--- crates/apu/src/blipbuf.rs
+++ crates/apu/src/blipbuf.rs
@@ -1,7 +1,7 @@
 pub(crate) use blip_buf::BlipBuf;
 
-pub fn new(freqency: u32, sample_rate: u32) -> BlipBuf {
+pub fn new(frequency: u32, sample_rate: u32) -> BlipBuf {
     let mut blipbuf = BlipBuf::new(sample_rate);
-    blipbuf.set_rates(f64::from(freqency), f64::from(sample_rate));
+    blipbuf.set_rates(f64::from(frequency), f64::from(sample_rate));
     blipbuf
 }
