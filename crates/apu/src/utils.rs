08b8878f46eff651375b872b8aa32823e4181098
nomyfan <Nomyfan@live.com>
2024-03-04 18:49:48 +0800

    wip: add debug assert

--- crates/apu/src/utils.rs
+++ crates/apu/src/utils.rs
@@ -1,5 +1,7 @@
 /// Convet frequency to clock cycles.
 #[inline]
 pub(crate) const fn freq_to_clock_cycles(freq: u32) -> u32 {
+    debug_assert!(freq <= gb_shared::CPU_FREQ);
+
     gb_shared::CPU_FREQ / freq
 }
