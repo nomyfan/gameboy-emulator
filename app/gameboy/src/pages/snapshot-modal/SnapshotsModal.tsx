fb4465cbba30d62a32f9e0cf8dc41bd6f2d05121
nomyfan <Nomyfan@live.com>
2024-04-23 15:13:33 +0800

    fix: quick pressing buttons has no effects on some devices

--- app/gameboy/src/pages/snapshot-modal/SnapshotsModal.tsx
+++ app/gameboy/src/pages/snapshot-modal/SnapshotsModal.tsx
@@ -1,4 +1,4 @@
-import * as Dialog from "@radix-ui/react-dialog";
+import { Dialog, DialogPortal, DialogContent } from "@radix-ui/react-dialog";
 import { Snapshots } from "gameboy/components/snapshots";
 import { actions, store } from "gameboy/store";
 import { useStore } from "zustand";
@@ -9,18 +9,19 @@ export function SnapshotsModal() {
   const open = useStore(store, (st) => st.ui.snapshotModalOpen);
 
   return (
-    <Dialog.Root open={open}>
-      <Dialog.Portal>
-        <Dialog.Overlay
-          onClick={() => {
-            actions.toggleSnapshotModal(false);
-          }}
-          className={styles.overlay}
-        />
-        <Dialog.Content style={{ pointerEvents: "auto" }}>
-          <div className={styles.drawer}>{open && <Snapshots />}</div>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
+    <Dialog open={open}>
+      <DialogPortal>
+        <DialogContent>
+          <div
+            onClick={() => {
+              actions.toggleSnapshotModal(false);
+            }}
+            className={styles.overlay}
+          >
+            <div className={styles.drawer}>{open && <Snapshots />}</div>
+          </div>
+        </DialogContent>
+      </DialogPortal>
+    </Dialog>
   );
 }
