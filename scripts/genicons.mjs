8c76b163372ea82cbddc38bec450fb260a193c13
nomyfan <Nomyfan@live.com>
2024-04-27 00:48:39 +0800

    chore(ci): skip installing optional deps

---
+++ scripts/genicons.mjs
@@ -0,0 +1,30 @@
+import sharp from "sharp";
+import ico from "sharp-ico";
+import * as path from "node:path";
+
+const root = path.resolve(
+  import.meta.dirname,
+  "..",
+  "app",
+  "gameboy",
+  "public",
+);
+
+const options = [
+  { size: 64, name: "pwa-64x64.png" },
+  { size: 180, name: "apple-touch-icon-180x180.png" },
+  { size: 192, name: "pwa-192x192.png" },
+  { size: 512, name: "pwa-512x512.png" },
+];
+
+for (const { size, name } of options) {
+  await sharp(path.join(root, "maskable-icon-512x512.png"))
+    .resize(size)
+    .toFile(path.join(root, name));
+}
+
+await ico.sharpsToIco(
+  [sharp(path.join(root, "maskable-icon-512x512.png"))],
+  path.join(root, "favicon.ico"),
+  { sizes: [48], resizeOptions: {} },
+);
