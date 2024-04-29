47ae840ca8e2187fad5751705d222956bf310811
nomyfan <Nomyfan@live.com>
2024-04-29 23:55:04 +0800

    feat: change icon

--- scripts/genicons.mjs
+++ scripts/genicons.mjs
@@ -7,24 +7,27 @@ const root = path.resolve(
   "..",
   "app",
   "gameboy",
-  "public",
+  "public"
 );
+
+const mainIcon = "maskable-icon.svg";
 
 const options = [
   { size: 64, name: "pwa-64x64.png" },
   { size: 180, name: "apple-touch-icon-180x180.png" },
   { size: 192, name: "pwa-192x192.png" },
   { size: 512, name: "pwa-512x512.png" },
+  { size: 512, name: "maskable-icon-512x512.png" },
 ];
 
 for (const { size, name } of options) {
-  await sharp(path.join(root, "maskable-icon-512x512.png"))
+  await sharp(path.join(root, mainIcon))
     .resize(size)
     .toFile(path.join(root, name));
 }
 
 await ico.sharpsToIco(
-  [sharp(path.join(root, "maskable-icon-512x512.png"))],
+  [sharp(path.join(root, mainIcon))],
   path.join(root, "favicon.ico"),
-  { sizes: [48], resizeOptions: {} },
+  { sizes: [48], resizeOptions: {} }
 );
