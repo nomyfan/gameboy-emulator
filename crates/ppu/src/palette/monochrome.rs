4e4d3d89d47b36926d5307566f2591f6fe6c458f
nomyfan <Nomyfan@live.com>
2024-05-05 22:58:30 +0800

    feat: reduce struct fields

--- crates/ppu/src/palette/monochrome.rs
+++ crates/ppu/src/palette/monochrome.rs
@@ -8,40 +8,31 @@ pub(crate) struct Monochrome {
     bgp: u8,
     obp0: u8,
     obp1: u8,
-    bgp_palette: [u32; 4],
-    obp0_palette: [u32; 4],
-    obp1_palette: [u32; 4],
     colors: [[u32; 4]; 3],
 }
 
 impl Monochrome {
     pub(crate) fn new(palette_id: u16) -> Self {
-        let [bgp_palette, obp0_palette, obp1_palette]: [[u32; 4]; 3] =
-            match find_palette(palette_id) {
-                Some(p) => {
-                    [[p[0], p[1], p[2], p[3]], [p[4], p[5], p[6], p[7]], [p[8], p[9], p[10], p[11]]]
-                }
-                None => [*COLORS, *COLORS, *COLORS],
-            };
+        let colors = find_palette(palette_id).map_or([*COLORS, *COLORS, *COLORS], |p| {
+            [[p[0], p[1], p[2], p[3]], [p[4], p[5], p[6], p[7]], [p[8], p[9], p[10], p[11]]]
+        });
 
-        let colors = [bgp_palette, obp0_palette, obp1_palette];
-
-        Self { bgp: 0xFC, obp0: 0, obp1: 0, bgp_palette, obp0_palette, obp1_palette, colors }
+        Self { bgp: 0xFC, obp0: 0, obp1: 0, colors }
     }
 }
 
 impl Palette for Monochrome {
     fn background_color(&self, _palette_id: u8, color_id: u8) -> u32 {
-        self.bgp_palette[((self.bgp >> (color_id * 2)) & 0b11) as usize]
+        self.colors[0][((self.bgp >> (color_id * 2)) & 0b11) as usize]
     }
 
     fn object_color(&self, palette_id: u8, color_id: u8) -> u32 {
         if palette_id == 0 {
             let obp = self.obp0;
-            self.obp0_palette[((obp >> (color_id * 2)) & 0b11) as usize]
+            self.colors[1][((obp >> (color_id * 2)) & 0b11) as usize]
         } else {
             let obp = self.obp1;
-            self.obp1_palette[((obp >> (color_id * 2)) & 0b11) as usize]
+            self.colors[2][((obp >> (color_id * 2)) & 0b11) as usize]
         }
     }
 
