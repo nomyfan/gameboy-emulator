c1ed61af13db67aa80eb282f137bdc03eff3af86
nomyfan <Nomyfan@live.com>
2024-05-06 23:16:35 +0800

    feat: reduce built WASM file size ~4KB

--- crates/ppu/src/vram.rs
+++ crates/ppu/src/vram.rs
@@ -1,5 +1,6 @@
-use gb_shared::{boxed::BoxedArray, is_bit_set, MachineModel, Memory, Snapshot};
+use gb_shared::{is_bit_set, MachineModel, Memory, Snapshot};
 
+#[derive(serde::Serialize, serde::Deserialize)]
 pub(crate) struct VideoRam {
     /// Tile data area(in size of 0x1800).
     /// There are total 384 tiles, each tile has 16 bytes.
@@ -20,7 +21,7 @@ pub(crate) struct VideoRam {
     /// Tile map area(in size of 0x800).
     /// - Tile map 0: 0x9800-0x9BFF
     /// - Tile map 1: 0x9C00-0x9FFF
-    ram: Vec<BoxedArray<u8, 0x2000>>,
+    ram: Vec<u8>,
     /// a.k.a VBK. On CGB, there're two banks.
     bank_num: u8,
 }
@@ -29,8 +30,8 @@ impl VideoRam {
     pub(crate) fn new(machine_model: MachineModel) -> Self {
         Self {
             ram: match machine_model {
-                MachineModel::DMG => vec![Default::default()],
-                MachineModel::CGB => vec![Default::default(), Default::default()],
+                MachineModel::DMG => vec![0x00; 0x2000],
+                MachineModel::CGB => vec![0x00; 0x4000],
             },
             bank_num: 0,
         }
@@ -40,18 +41,21 @@ impl VideoRam {
 impl VideoRam {
     /// `nth` is in range of 0..=383.
     pub(crate) fn tile(&self, bank_num: u8, index: usize) -> &[u8; 16] {
-        let offset = index * 16;
-        let bank = &self.ram[bank_num as usize];
+        let offset = index * 16 + (bank_num as usize * 0x2000);
 
-        bank[offset..(offset + 16)].as_ref().try_into().unwrap()
+        self.ram[offset..(offset + 16)].as_ref().try_into().unwrap()
     }
 
     pub(crate) fn tile_index(&self, index: usize) -> u8 {
-        self.ram[0][index + 0x1800]
+        self.ram[index + 0x1800]
     }
 
     pub(crate) fn bgw_tile_attrs(&self, index: usize) -> Option<BackgroundAttrs> {
-        self.ram.get(1).map(|bank| BackgroundAttrs(bank[index + 0x1800]))
+        if self.ram.len() == 0x2000 {
+            None
+        } else {
+            Some(BackgroundAttrs(self.ram[0x3800 + index]))
+        }
     }
 
     pub(crate) fn bgw_tile_info(&self, index: usize) -> (u8, Option<BackgroundAttrs>) {
@@ -63,7 +67,7 @@ impl Memory for VideoRam {
     fn write(&mut self, addr: u16, value: u8) {
         match addr {
             0x8000..=0x9FFF => {
-                self.ram[self.bank_num as usize][addr as usize - 0x8000] = value;
+                self.ram[self.bank_num as usize * 0x2000 + (addr as usize - 0x8000)] = value;
             }
             0xFF4F => self.bank_num = value & 0x01,
             _ => unreachable!("Invalid VRAM write at {:#X} {:#X}", addr, value),
@@ -72,37 +76,25 @@ impl Memory for VideoRam {
 
     fn read(&self, addr: u16) -> u8 {
         match addr {
-            0x8000..=0x9FFF => self.ram[self.bank_num as usize][addr as usize - 0x8000],
+            0x8000..=0x9FFF => self.ram[self.bank_num as usize * 0x2000 + (addr as usize - 0x8000)],
             0xFF4F => self.bank_num | 0xFE,
             _ => unreachable!("Invalid VRAM read at {:#X}", addr),
         }
     }
 }
 
-#[derive(serde::Serialize, serde::Deserialize)]
-pub(crate) struct VideoRamSnapshot {
-    ram: Vec<u8>,
-    bank_num: u8,
-}
+pub(crate) type VideoRamSnapshot = VideoRam;
 
 impl Snapshot for VideoRam {
     type Snapshot = VideoRamSnapshot;
 
     fn take_snapshot(&self) -> Self::Snapshot {
-        let mut ram_snapshot = Vec::with_capacity(0x2000 * self.ram.len());
-        for bank in &self.ram {
-            ram_snapshot.extend_from_slice(bank.as_ref());
-        }
-
-        Self::Snapshot { ram: ram_snapshot, bank_num: self.bank_num }
+        VideoRamSnapshot { ram: self.ram.clone(), bank_num: self.bank_num }
     }
 
     fn restore_snapshot(&mut self, snapshot: Self::Snapshot) {
-        assert_eq!(snapshot.ram.len(), 0x2000 * self.ram.len());
-
-        self.ram.iter_mut().zip(snapshot.ram.chunks(0x2000)).for_each(|(bank, chunk)| {
-            bank.copy_from_slice(chunk);
-        });
+        assert_eq!(self.ram.len(), snapshot.ram.len());
+        self.ram = snapshot.ram;
         self.bank_num = snapshot.bank_num;
     }
 }
