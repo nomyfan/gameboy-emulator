49201c3c33346c2ebf707248a9d1379606ef15ac
nomyfan <Nomyfan@live.com>
2024-06-13 11:07:40 +0800

    feat: better type checking
    the first parameter of the callback cannot be `null`

--- app/gameboy/src/hooks/useRefCallback.ts
+++ app/gameboy/src/hooks/useRefCallback.ts
@@ -1,19 +1,25 @@
 import type { DependencyList } from "react";
 import { useCallback, useRef } from "react";
 
+type FirstParameterNonNull<T> = T extends (value: infer V) => infer R
+  ? [V] extends [Exclude<V, null>]
+    ? (value: V) => R
+    : never
+  : never;
+
 /**
  * TODO: we can remove this hook when it's GA.
  * @see https://react.dev/reference/react-dom/components/common#returns
  * @param callback
  * @param deps
  */
-export function useRefCallback<T, V = Exclude<T, null>>(
-  callback: (value: V) => void | (() => void),
+export function useRefCallback<T>(
+  callback: FirstParameterNonNull<(value: T) => void | (() => void)>,
   deps?: DependencyList,
 ) {
   const dispose = useRef<() => void>();
   return useCallback(
-    (value: V | null) => {
+    (value: T | null) => {
       if (value !== null) {
         dispose.current = callback(value) || undefined;
       } else {
